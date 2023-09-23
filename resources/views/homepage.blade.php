3cc54a358ec54a56e3ba5355bc9a8c5cc76f2ea7
Mushfiq-Alam71 <mushfiq9912@gmail.com>
2023-09-23 22:49:23 +0600

    first commit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,23 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Homepage</title>
+    <link href="{{asset('css/style.css') }}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Skranji&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
+    <div>
+        <div>
+            <h1 class="logo">WebWire Tribune</h1>
+        </div>
+    </div>
+    <nav class="navbar">
+        
+        <div class="nav-item">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Contact</a></li>
+        </div>
+        <div class="navbar-side">
+            <li><a href="#">login</a></li>
+            <li><a href="#">Registration</a></li>
+        </div>
+    </nav>
+    
+    
     <div class="container">
-        
-        <nav class="">
-            <h1>mushfiq alam sami</h1>
-        </nav>
-        
+        <div class="top-container">
+            <div class="popular">
+                <div class="popular-title"><h1>Trending Now</h1></div>
+                <div class="popular-news"></div>
+            </div>
 
-        <div class="body-container">
+            <div class="body-container">
+                <h1>The Daily Star</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias, veritatis error ea eveniet iste corporis fuga atque commodi dolor repudiandae quod veniam fugiat vero doloribus optio sit quos soluta quae. Accusamus ab iusto eveniet labore dignissimos fugiat unde harum, quo dolores quae veritatis tempore tempora. Omnis incidunt, qui tenetur nisi architecto, voluptas, reprehenderit consequatur voluptates dolores eligendi dolorem odio earum accusamus eius accusantium est vero iure? Doloribus, labore! Fugit iure quos repudiandae reiciendis nam deleniti consectetur voluptas deserunt sunt facilis dolores necessitatibus voluptatum iusto, blanditiis eligendi sequi dolorem! Unde repudiandae tempora fugiat quos eos quibusdam suscipit sequi? Quis, laboriosam?</p>
 
+                <h1>Prothom Alo</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi illum dicta nostrum asperiores voluptate in nobis architecto incidunt ullam enim accusantium nesciunt, culpa sit cumque ipsa itaque voluptates hic unde perspiciatis magni placeat provident aliquam? Corrupti, esse, facere rem deleniti est illum nam, sapiente perspiciatis reprehenderit illo unde! Suscipit explicabo adipisci, consequatur nihil magnam odio dignissimos labore voluptatum ipsa temporibus natus nemo. Corrupti, perferendis. Exercitationem molestiae perspiciatis nisi optio omnis porro in fuga architecto. Ad excepturi modi perspiciatis ab esse odit voluptatem culpa reprehenderit, eos eum aspernatur tenetur autem, neque corrupti tempora beatae assumenda reiciendis magni. Tenetur quia consequuntur ipsam!</p>
+            </div>
         </div>
 
 
@@ -28,4 +59,5 @@
 
 
 </body>
+
 </html>
