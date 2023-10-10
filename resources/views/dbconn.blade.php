e23ab15e773cc156ff038547567282a0a6166481
Nafees Ashker Sakir <sakir.ashker@gmail.com>
2023-10-11 00:11:42 +0600

    database

--- resources/views/dbconn.blade.php
+++ resources/views/dbconn.blade.php
@@ -3,25 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-=======
     <title>Document</title>
 </head>
 <body>
         <?php
-        use App\Models\UserInfo;
+        use App\Models\User;
 
-        $data = UserInfo::all();
+        $data = User::all();
         
             if(DB::connection()->getPdo()){
                 echo $data[0]->email;
             }
         ?>
->>>>>>> 0ba6e9b (database)
 </body>
 </html>
